85d11675a33948e41806781c546310abf82ce833
Peter Gorgone <peter@peltronic.com>
2019-09-30 17:25:20 -0700

    Impl image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -9,4 +9,11 @@ class Portfolio < ApplicationRecord
   # Custom Scope (2), using labmdas
   scope :ruby_on_rails, ->  { where(subtitle: 'Ruby on Rails') } 
 
+  after_initialize :set_defaults
+
+  # a callback
+  def set_defaults
+    self.main_image ||= "https://via.placeholder.com/600x400"
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
+  end
 end
