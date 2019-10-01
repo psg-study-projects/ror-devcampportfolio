ec3a53ea23d56d99ba0506efffa1df86756cd40d
Peter Gorgone <peter@peltronic.com>
2019-09-30 18:06:27 -0700

    Impl concernt for managing data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
   
   # Custom Scope (1)
@@ -13,7 +14,7 @@ class Portfolio < ApplicationRecord
 
   # a callback
   def set_defaults
-    self.main_image ||= "https://via.placeholder.com/600x400"
-    self.thumb_image ||= "https://via.placeholder.com/350x200"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
