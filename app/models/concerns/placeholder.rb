18fdef4812f73e3f84554b9fb3baa83ed6bdfb7b
Peter Gorgone <peter@peltronic.com>
2020-04-16 11:00:55 -0700

    [INIT] -- use slug for show etc routes for project resource

--- app/models/concerns/placeholder.rb
+++ app/models/concerns/placeholder.rb
@@ -1,6 +1,7 @@
 module Placeholder
   extend ActiveSupport::Concern
 
+  # basically a helper method
   def self.image_generator(height:, width:)
     "https://via.placeholder.com/#{height}x#{width}"
   end
